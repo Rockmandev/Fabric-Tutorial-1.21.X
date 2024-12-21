8f9da865432d93b634022cb27c3cf347c6b2af1c
Toby Wolff <tobias-git@wolffh.de>
2024-12-21 11:21:49 +1100

    Initial commit

---
+++ src/main/java/net/toby/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.toby.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "TutorialMod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
