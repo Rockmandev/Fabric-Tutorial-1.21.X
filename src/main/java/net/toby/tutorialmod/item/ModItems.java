1450fee5f6957187da37677ec542835f0e79f322
Toby Wolff <tobias-git@wolffh.de>
2024-12-28 16:56:55 +1100

    removed unnecessary imports

--- src/main/java/net/toby/tutorialmod/item/ModItems.java
+++ src/main/java/net/toby/tutorialmod/item/ModItems.java
@@ -1,9 +1,7 @@
 package net.toby.tutorialmod.item;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.toby.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
